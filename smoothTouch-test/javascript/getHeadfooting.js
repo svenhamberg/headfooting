99a69fe82fb2ef0caa204029654e35d009d8ad05
Sven Hamberg <hamberg.sven@gmail.com>
2014-03-04 22:14:06 +0100

    try smoothtouch

---
+++ smoothTouch-test/javascript/getHeadfooting.js
@@ -0,0 +1,14 @@
+function getHeadfooting(headfootingIndex)
+	{
+
+		headfooting=xmlDoc.getElementsByTagName("headfooting").item(headfootingIndex);
+
+		image=headfooting.getElementsByTagName("image")[0].childNodes[0].nodeValue;
+
+		category=headfooting.getElementsByTagName("category")[0].childNodes[0].nodeValue;
+
+		artist=headfooting.getElementsByTagName("artist")[0].childNodes[0].nodeValue;
+
+		document.getElementById("headfooting_"+headfootingIndex).innerHTML="<img src=\""+image+"\">"+"<p>#"+category+"</p>";
+		
+	}
